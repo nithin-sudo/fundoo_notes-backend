86f55fe9bb326331017484f63cd6bd126fcce389
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-10-05 11:54:51 +0530

    Added: migrations for label table.

---
+++ database/migrations/2021_10_04_045123_create_labels_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('labels', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('note_id');
+            $table->string('labelname');
+            $table->foreign('note_id')
+                ->references('id')
+                ->on('notes')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->enum('notes', [0, 1, 2])->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('labels');
+    }
+}
