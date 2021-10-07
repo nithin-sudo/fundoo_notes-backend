26c9d9ca7c13b8015d2c4e53ad4bd7d980aaa6c7
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-10-07 11:24:38 +0530

    Added:relations to another controller.

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -11,4 +11,14 @@ class Label extends Model
 
     protected $table="labels";
     protected $fillable = ['labelname'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function note()
+    {
+        return $this->belongsTo(Note::class);
+    }
 }
