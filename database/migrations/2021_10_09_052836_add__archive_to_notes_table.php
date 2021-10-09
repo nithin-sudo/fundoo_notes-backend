343eef6d847f454654cb90c17c27cd23d7030def
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-10-09 12:00:42 +0530

    Added: new column migrations.

---
+++ database/migrations/2021_10_09_052836_add__archive_to_notes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddArchiveToNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            //
+            $table->boolean('Archive')->default(0)->after('pin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            //
+            $table->dropColumn('pin');
+        });
+    }
+}
